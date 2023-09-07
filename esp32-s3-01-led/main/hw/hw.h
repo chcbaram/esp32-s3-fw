917b1dcf0a27b383671003646ca41426551ae73a
Hancheol Cho <chcbaram@gmail.com>
2023-09-07 21:58:25 +0900

    LED 예제 추가

---
+++ esp32-s3-01-led/main/hw/hw.h
@@ -0,0 +1,32 @@
+/*
+ * hw.h
+ *
+ *  Created on: 2021. 1. 9.
+ *      Author: baram
+ */
+
+#ifndef MAIN_HW_HW_H_
+#define MAIN_HW_HW_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "hw_def.h"
+
+
+#include "led.h"
+#include "uart.h"
+#include "cli.h"
+#include "log.h"
+#include "cdc.h"
+
+
+void hwInit(void);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* MAIN_HW_HW_H_ */
